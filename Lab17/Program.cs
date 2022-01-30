5f9181dc17eb4ae503042a45e13ae2d7e7f6569d
Юлия Шепелева <9128051206@mail.ru>
2022-01-30 14:46:50 +0500

    Добавьте файлы проекта.

---
+++ Lab17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер расчетного счета");
+            int numint = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс расчетного счета");
+            int balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            string name = Console.ReadLine();
+            
+            Score<int> score1 = new Score<int>(numint, balance, name);
+            Console.WriteLine(score1.GetInfo());
+
+            Console.WriteLine("Введите строковый номер расчетного счета");
+            string numstring = Console.ReadLine();
+            Console.WriteLine("Введите баланс расчетного счета");
+            balance = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            name = Console.ReadLine();
+            
+            Score<string> score2 = new Score<string>(numstring, balance, name);
+            Console.WriteLine(score2.GetInfo());
+            Console.ReadKey();
+        }        
+    }
+}
---
+++ Lab17/Score.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class Score <T>
+    {
+        public T Num { get; set; }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+
+        public Score(T num, int balance, string name)
+        {
+            Num = num;
+            Balance = balance;
+            Name = name;
+        }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Balance} {Name}";
+        }
+    }
+}
